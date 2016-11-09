d9d253817b04c9e069a3163c4e51e64c75e65316
Marco Antonio Valenzuela Escárcega <marcos.vzla@gmail.com>
2016-11-09 10:50:53 -0700

    wip

--- examples/random_pong.py
+++ examples/random_pong.py
@@ -2,10 +2,9 @@ import gym
 
 env = gym.make('Pong-v0')
 env.reset()
-env.render()
 while True:
+    env.render()
     action = env.action_space.sample()
     obs, reward, done, _ = env.step(action)
     print reward, done
-    env.render()
     if done: break
